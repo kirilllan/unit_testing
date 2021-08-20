bce465e3cb2f8dd3862cd611664f400daa1b7f53
kirilllan <s2100138@edu.bc.fi>
2021-08-20 10:32:48 +0300

    arr of tests,another syntax

--- calcproject/__test__/calclibrary.test.js
+++ calcproject/__test__/calclibrary.test.js
@@ -10,5 +10,16 @@ describe("Test sum with integers", () => {
 	test('test 2 + 3 = 5', () => {
 		expect(sum(2, 3)).toBe(5);
 	});
+});
 
+describe('Test sum with integers using test.each', () => {
+	const testValues = [
+		//a  b  expected 
+		[1, 1, 2],
+		[2, 3, 5],
+		[-2, -4, -6]
+	];
+	test.each(testValues)('sum(%s, %s) = %s', (a, b, expected) => {
+		expect(sum(a, b)).toBe(expected);
+	});
 });
