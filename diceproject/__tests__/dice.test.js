bd8d883981313be88c9ff08f4c32b11cc0823344
kirilllan <s2100138@edu.bc.fi>
2021-08-27 11:42:16 +0300

    3 tests for dots amount

--- diceproject/__tests__/dice.test.js
+++ diceproject/__tests__/dice.test.js
@@ -1,3 +1,18 @@
 'use strict'
 
 const Dice = require('../dice.js')
+
+describe('Create a dice with no upper bound given', () => {
+  const dice = new Dice()
+
+  test('maximum dot count is 6', () => {
+    expect(dice.maximumValue).toBe(6)
+  })
+  test('minimum dot count is 1', () => {
+    expect(dice.minimumValue).toBe(1)
+  })
+  test('dots is 0', () => {
+    expect(dice.dots).toBe(0)
+  })
+
+})
