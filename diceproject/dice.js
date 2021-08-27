e953e5278edc2605adeeb6e575169fbd013aa820
kirilllan <s2100138@edu.bc.fi>
2021-08-27 13:08:02 +0300

    Dice constructor&3 getters

--- diceproject/dice.js
+++ diceproject/dice.js
@@ -1,5 +1,17 @@
 'use strict'
 
 module.exports = class Dice {
-
+  constructor(upperBound) {
+    this.upperBound = upperBound
+    this.dotCount = 0
+  }
+  get dots() {
+    return this.dotCount
+  }
+  get maximumValue() {
+    return this.upperBound
+  }
+  get minimumValue() {
+    return 1
+  }
 }
