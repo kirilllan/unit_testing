8d28a3017bfe6477a7fe1ce3fd69ff3b01072955
kirilllan <s2100138@edu.bc.fi>
2021-09-12 12:11:13 +0300

    test 6 & 7

--- my_name_unit_test_task/__tests__/test.js
+++ my_name_unit_test_task/__tests__/test.js
@@ -32,6 +32,16 @@ test('test if there are accessories in product id 2', () => {
   expect(new Prod(prods).hasAccessories(2)).toBe(true)
 })
 
+// test 6 GetProductAccessories(id)
+test('id 3 has no accessories should be true', () => {
+  expect(new Prod(prods).GetProductAccessories(3)).toEqual([])
+})
+
+//test 7 getPriceWithoutExtras(id)
+test('id 2 price is 99', () => {
+  expect(new Prod(prods).getPriceWithoutExtras(2)).toEqual(99)
+})
+
 //console.log(new Prod(3))
 describe("this tests for an exceptiom", () => {
   const testFunction = new Prod(101010)
